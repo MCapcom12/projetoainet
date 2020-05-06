d0691270cd38773e313804c3be3fb26d358ea1db
Ricardos29 <62677696+Ricardos29@users.noreply.github.com>
2020-05-07 00:42:09 +0100

    Login
    Criei um user

--- public/js/app.js
+++ public/js/app.js
@@ -37393,8 +37393,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\AInet\www\projetoainet\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\AInet\www\projetoainet\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\ainet_2020\public\projetoainet\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\ainet_2020\public\projetoainet\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
