d6ea4baae5b4e0c0863eb8b0c1ca92122d584013
hugodomingues <62695616+hugodomingues@users.noreply.github.com>
2020-05-07 10:46:18 +0100

    index contas

---
+++ resources/views/contas/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layout_admin')
+
+@section('content')
+    <h2>Contas</h2>
+    <form class="disc-search" action="#" method="GET">
+        <div class="search-item">
+            <label for="idUser">Uuser:</label>
+            <select name="user" id= "idCurso">
+                
+@endsection
