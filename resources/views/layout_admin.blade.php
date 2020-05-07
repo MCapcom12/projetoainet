5dee7575d8c333feec19cc2b41f291674f8d4eaf
Ricardos29 <62677696+Ricardos29@users.noreply.github.com>
2020-05-07 09:40:59 +0100

    Register
    Meti a parte do register visivel

--- resources/views/layout_admin.blade.php
+++ resources/views/layout_admin.blade.php
@@ -125,6 +125,10 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                 </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
             @else 
                 <!-- Nav Item - User Information -->
                 <li class="nav-item dropdown no-arrow">
