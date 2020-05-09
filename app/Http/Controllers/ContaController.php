238f76593a849d4103fe0626a249b4c8c7b47ffc
hugodomingues <62695616+hugodomingues@users.noreply.github.com>
2020-05-09 19:10:31 +0100

    ver contas

--- app/Http/Controllers/ContaController.php
+++ app/Http/Controllers/ContaController.php
@@ -4,11 +4,50 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Conta;
+use App\User;
 
 class ContaController extends Controller
 {
-    public function admin(){
-        $contas = Conta::all();
-        return view('contas.admin')->withContas($contas);
+    public function admin(Request $request){
+       $user= $request->user ?? '';
+       
+       $qry= Conta::query();
+
+       if($user){
+           $qry->where('user',$user);
+       }
+       $contas=$qry->paginate(10);
+       $users = User::pluck('name','id');
+
+       return view('contas.admin')
+        ->withContas($contas)
+        ->withUsers($users)
+        ->withSelectedUser($user);
+
+        
     }
+
+    public function edit(){
+        return view('contas.edit');
+    }
+
+    public function create(){
+        $listaUsers= User::pluck('name','id');
+        return view('contas.create')->withUsers($listaUsers);
+    }
+
+    public function store(Request $request){
+        $validated_data = $request->validade([
+            'nome'=>'required|string|max:20'
+
+        ]);
+
+        Conta::create($validated_data);
+
+        return redirect()->route('admin.disciplinas')
+            ->with('alert-msg','Conta criada com sucesso')
+            ->with('alert-type','success');
+    }
+
+   
 }
--- resources/views/contas/admin.blade.php
+++ resources/views/contas/admin.blade.php
@@ -1,33 +1,47 @@
 @extends('layout_admin')
+@section('title','Contas')
 
 @section('content')
-    <table>
-        <thead>
-            <tr>
-                <th>id</th>
-                <th>user_id</th>
-                <th>nome</th>
-                <th>descricao</th>
-                <th>saldo abertura</th>
-                <th>saldo atual</th>
-                <th>data ultimo movimento</th>
-                <th>delete at</th>
-            </tr>
-        </thead>
-        <tbody>
-        @foreach($contas as $cont)
-            <tr>
-                <td> {{$cont->id}}</td>
-                <td> {{$cont->user_id}}</td>
-                <td> {{$cont->nome}}</td>
-                <td> {{$cont->descricao}}</td>
-                <td> {{$cont->saldo_abertura}}</td>
-                <td> {{$cont->saldo_atual}}</td>
-                <td> {{$cont->data_ultimo_movimento}}</td>
-                <td> {{$cont->delete_at}}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-
+<div class= "row mb-3">
+    <div class="col-3">
+        <a href="{{route('admin.contas.create')}}" class="btn btn-success" role="button" aria-pressed="true">Nova Conta</a>
+    </div>
+    <div class="col-9">
+        <form method="GET" action="{{route('admin.contas')}}" class="form-group">
+            <div class="input-group">
+                <Select class="custom-select" name="user" id="inputUser" aria-label="User">
+                    <option value= "" {{''== old('user', $selectedUser) ? 'selected' : '' }}>Todos Users</option>
+                    @foreach($users as $abr => $nome)
+                    <option value={{$abr}} {{$abr== old('user', $selectedUser) ? 'selected' : ''}}>{{$nome}}</option>
+                    @endforeach
+                </Select>
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="submit">Filtrar</button>
+                </div>
+            </div>
+        </form>
+    </div>    
+</div> 
+<table class = "table">
+    <thead>
+        <tr>          
+            <th>nome</th>            
+            <th>saldo atual</th> 
+            <th></th>
+            <th></th>           
+        </tr>
+    </thead>
+    <tbody>
+    @foreach($contas as $cont)
+        <tr>            
+            <td> {{$cont->nome}}</td>
+            <td> {{$cont->saldo_atual}}</td>
+            <td><a href="{{route('admin.contas.edit', ['id' =>$cont->id])}}" class="btn btn-primary btn-sm" role="button" aria-pressed ="true">Alterar </a></td>
+            <td>
+                <a href="#" class= "btn btn-danger btn-sm">Apagar(Ainda por fazer)</a> 
+            </td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
 @endsection
