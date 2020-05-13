1d336c48523c70b175791df78b00475b6d18f4f8
MCapcom12 <33904864+MCapcom12@users.noreply.github.com>
2020-05-13 11:20:25 +0100

    Perfil Bonitinho + Imagem no dropdown menu

--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -1,10 +1,20 @@
 @extends('layout_admin')
 
-@section('title','PERFIL')
-
 @section('content')
-<img src="{{$path = '/storage/fotos/' . $id->id . '/' . $id->foto}}"  alt="" style="width:300px;height:auto;float:left;">
+
+<div class="container">
+	<div calss="row">
+		<div class="col-md-10 col-md-offset1">
+			<img src="{{$id->foto ? $path = '/storage/fotos/' . $id->id . '/' . $id->foto : asset('img/default_img.png') }}" style="width:150px;height:150px;float:left; border-radius: 50%; margin-right: 25px;">
+			<h2>Perfil</h2>
+			<h2>{{$id->name}}</h2>
+			<label>{{$id -> email}}</label>
+		</div>
+	</div>
+</div>
+
+<!--
 <h2>Nome: {{$id -> name}}</h2>
-<h2>Email: {{$id -> email}}</h2>
+<h2>Email: {{$id -> email}}</h2>-->
 
 @endsection
