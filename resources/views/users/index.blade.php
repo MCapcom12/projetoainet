f4e982ab21a55d260b41ba54b6322b2ddd305c95
MCapcom12 <33904864+MCapcom12@users.noreply.github.com>
2020-05-14 01:42:14 +0100

    Lista de users (Não funcional)

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,4 +2,28 @@
 @section('title','UTILIZADORES')
 @section('content')
 
+</div> 
+<table class = "table">
+    <thead>
+        <tr>
+            <th>id</th>
+            <th>nome</th>
+            <th>email</th> 
+            <th></th>
+            <th></th> 
+            <th></th> 
+        </tr>
+    </thead>
+    <tbody>
+    @foreach($users as $id)
+        <tr>   
+            <td>{{$id->id}}</td>         
+            <td>{{$id->name}}</td>
+            <td>{{$id->email}}</td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
+</div>
+
 @endsection
