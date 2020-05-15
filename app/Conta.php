10add5c74b206157720ef54f4c73f3c1963a6707
hugodomingues <62695616+hugodomingues@users.noreply.github.com>
2020-05-15 22:18:55 +0100

    Update Conta.php

--- app/Conta.php
+++ app/Conta.php
@@ -3,13 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Auth;
 
 class Conta extends Model
 {
+    
+
     public $timestamps = false;
    
     protected $fillable=['nome','user_id','descricao','saldo_abertura','saldo_atual','data_ultimo_movimento','deleted_at'];
 
-    
+    public function __construct(array $attributes = [])
+    {
+        $this->user_id = Auth::id();
+        $this->saldo_atual = $this->saldo_abertura;
+     
+        parent::__construct($attributes);
+    }
     
 }
